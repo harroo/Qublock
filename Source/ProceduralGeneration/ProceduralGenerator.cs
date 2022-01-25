3b31842eb3210c0b10072be7ac0d006ef558869c
harroo <harroo@protonmail.com>
2022-01-25 16:35:33 +1100

    do simple test for biomes

--- Source/ProceduralGeneration/ProceduralGenerator.cs
+++ Source/ProceduralGeneration/ProceduralGenerator.cs
@@ -11,6 +11,8 @@ namespace Qublock.ProceduralGeneration {
 
     public static class ProceduralGenerator {
 
+        // This is a Messy-Test!
+        // Replace the contents of this function with whatever you please.
         public static ChunkData GenerateChunkData (ChunkData chunk) {
 
             for (int x = 0; x < 32; ++x)
@@ -19,19 +21,64 @@ namespace Qublock.ProceduralGeneration {
                     int wx = chunk.position.x + x;
                     int wz = chunk.position.z + z;
 
-                    int height = Mathf.CeilToInt(
-                        // (float)noise.Evaluate(wx * 0.032f, 0, wz * 0.032f) * 8 + 32
-                        (
-                            SimplexNoise.Noise.Generate(wx * 0.032f, 0, wz * 0.032f) +
-                            SimplexNoise.Noise.Generate(wx * 0.016f, 0, wz * 0.016f) 
-                        ) * 8 + 32
-                    );
+                    switch ((int)(BiomeGd.BiomeFleck.Get(wx, wz) * 3)) {
 
-                    for (int y = 0; y < 32; ++y) {
+                        case 0: {
 
-                        int wy = chunk.position.y + y;
+                            int height = Mathf.CeilToInt(
+                                // (float)noise.Evaluate(wx * 0.032f, 0, wz * 0.032f) * 8 + 32
+                                (
+                                    SimplexNoise.Noise.Generate(wx * 0.032f, 0, wz * 0.032f) +
+                                    SimplexNoise.Noise.Generate(wx * 0.016f, 0, wz * 0.016f)
+                                ) * 2 + 32
+                            );
 
-                        if (wy < height) chunk[x, y, z] = 1;
+                            for (int y = 0; y < 32; ++y) {
+
+                                int wy = chunk.position.y + y;
+
+                                if (wy < height) chunk[x, y, z] = 1;
+                            }
+
+                        break; }
+
+                        case 1: {
+
+                            int height = Mathf.CeilToInt(
+                                // (float)noise.Evaluate(wx * 0.032f, 0, wz * 0.032f) * 8 + 32
+                                (
+                                    SimplexNoise.Noise.Generate(wx * 0.064f, 0, wz * 0.064f) +
+                                    SimplexNoise.Noise.Generate(wx * 0.016f, 0, wz * 0.016f)
+                                ) * 4 + 32
+                            );
+
+                            for (int y = 0; y < 32; ++y) {
+
+                                int wy = chunk.position.y + y;
+
+                                if (wy < height) chunk[x, y, z] = 1;
+                            }
+
+                        break; }
+
+                        case 2: {
+
+                            int height = Mathf.CeilToInt(
+                                // (float)noise.Evaluate(wx * 0.032f, 0, wz * 0.032f) * 8 + 32
+                                (
+                                    SimplexNoise.Noise.Generate(wx * 0.032f, 0, wz * 0.032f) +
+                                    SimplexNoise.Noise.Generate(wx * 0.016f, 0, wz * 0.016f)
+                                ) * 16 + 32
+                            );
+
+                            for (int y = 0; y < 32; ++y) {
+
+                                int wy = chunk.position.y + y;
+
+                                if (wy < height) chunk[x, y, z] = 1;
+                            }
+
+                        break; }
                     }
                 }
 
