5113260b786668057982cb0a48d2fa2a9ab4f118
harroo <harroo@protonmail.com>
2022-04-09 11:28:18 +1000

    expand raycast functionality

--- Source/CoreComponents/Interaction/QublockRaycast.cs
+++ Source/CoreComponents/Interaction/QublockRaycast.cs
@@ -13,6 +13,11 @@ namespace Qublock.Core.Interaction {
 
         public static bool Fire (Vector3 position, Vector3 rotation, out RayResult result, float range) {
 
+            return Fire(position, rotation, out result, range, 0.25f);
+        }
+
+        public static bool Fire (Vector3 position, Vector3 rotation, out RayResult result, float range, float jumpDistance) {
+
             float distanceTraveled = 0;
 
             position = Origin.UnityToOffset(position);
@@ -41,8 +46,8 @@ namespace Qublock.Core.Interaction {
                     previous = new GridCell(x, y, z, id);
                 }
 
-                position += rotation * 0.25f;
-                distanceTraveled += 0.25f;
+                position += rotation * jumpDistance;
+                distanceTraveled += jumpDistance;
             }
 
             result = new RayResult(GridCell.Zero, GridCell.Zero);
